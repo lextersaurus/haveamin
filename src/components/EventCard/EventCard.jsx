40b34868142cdf368e55df2c7b3a43242cbd61f0
Esther Mesa <estmesher@gmail.com>
2024-04-09 10:35:31 +0100

    HOME - Buttons are now functional (join/quit events)

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,41 +1,58 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
-import PlaceIcon from '@mui/icons-material/Place'
-import EventIcon from '@mui/icons-material/Event'
-import AccessibleIcon from '@mui/icons-material/Accessible'
-import CheckIcon from '@mui/icons-material/Check'
-import CloseIcon from '@mui/icons-material/Close'
-import PropTypes from 'prop-types'
-import './EventCard.css'
+import {
+    Place,
+    Event,
+    Accessible,
+    Check,
+    Close
+} from '@mui/icons-material'
 import { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import { getUserEvent } from '../../services/userService'
+import { joinEvent, quitEvent } from '../../services/eventService'
+
+import './EventCard.css'
 
 const EventCard = ({event}) => {
     const [events, setEvents] = useState([])
+    const [userJoin, setUserJoin] = useState()
 
     const handleUserEvents= async () => {
         const response = await getUserEvent()
         setEvents(response)
     }
 
+    const handleOnJoin = async (eventId) => {
+        await joinEvent(eventId)
+        setUserJoin(true)
+    }
+
+    const handleOnQuit = async (eventId) => {
+        await quitEvent(eventId)
+        setUserJoin(false)
+    }
+
     useEffect(() => {
         handleUserEvents()
-    }, [])
-
+    }, [userJoin])
+    
     const isUserJoined = events.some((userEvent) => userEvent.id === event.id)
 
   return (
     <Card color='background.paper' className="eventListLi" sx={{ minWidth: 275, maxWidth: 500 }} >
         <CardContent>
             <h3>{event.name}</h3>
-            <p><PlaceIcon sx={{ fontSize: 'medium'}}/> {event.place}</p>
-            <p><EventIcon sx={{ fontSize: 'medium'}}/> {event.date}</p>
+            <p><Place sx={{ fontSize: 'medium'}}/> {event.place}</p>
+            <p><Event sx={{ fontSize: 'medium'}}/> {event.date}</p>
             <p><b>Rango de edad: </b>{event.ageMin} - {event.ageMax}</p>
             {!event.isFree ? <p className='red'>Evento de pago</p> : <p className='green'>Evento gratuito</p>}
-            <div><AccessibleIcon /> {event.isAccessible ? <CheckIcon color='success'/> : <CloseIcon color='error'/>}</div>
+            <div><Accessible /> {event.isAccessible ? <Check color='success'/> : <Close color='error'/>}</div>
         </CardContent>
         <CardActions className='join-btn'>
             <Button color='secondary'>Ver detalles</Button>
-            {isUserJoined ? <Button variant='outlined'>Salir</Button> : <Button variant='contained'>Unirse</Button>}
+            {isUserJoined ?
+            <Button variant='outlined' onClick={() => {handleOnQuit(event.id)}}>Salir</Button> :
+            <Button variant='contained' onClick={() => {handleOnJoin(event.id)}}>Unirse</Button>}
         </CardActions>
     </Card>
   )
--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,25 +1,22 @@
 import { api } from "./config"
 
 export const getCategoryEvent = async () =>{
-    const { data } = await api.get('category/showall',{
+    const { data } = await api.get('/category/showall',{
             headers: {
                 'Authorization': localStorage.getItem('token')
             }
-        })/* 
-    console.log(data) */
+        })
     return data
- }
+}
 
- export const getAllEvents = async () =>{
-    const { data } = await api.get('event/showall',{
+export const getAllEvents = async () =>{
+    const { data } = await api.get('/event/showall',{
             headers: {
                 'Authorization': localStorage.getItem('token')
             }
-        })/* 
-    console.log(data) */
+        })
     return data
- }
-
+}
 
 export const getEventByCategory = async (categoryId) =>{
     const { data } = await api.get('/category/'+ categoryId +'/events',{
@@ -28,4 +25,22 @@ export const getEventByCategory = async (categoryId) =>{
         }
     })
     return data
- }
+}
+
+ export const joinEvent = async (eventId) => {
+    const { data } = await api.put(`/event/${eventId}/join`, {}, {
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
+    return data
+}
+
+export const quitEvent = async (eventId) => {
+    const { data } = await api.delete(`/event/${eventId}/quit`, {
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
+    return data
+}
