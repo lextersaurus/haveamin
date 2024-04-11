6922829e93949e179315b8032e9b7220a4a10b82
Esther Mesa <estmesher@gmail.com>
2024-04-11 04:13:36 +0100

    change createEvent styling

--- src/pages/CreateEvent/CreateEvent.jsx
+++ src/pages/CreateEvent/CreateEvent.jsx
@@ -1,16 +1,16 @@
 import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
 import 'dayjs/locale/es'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
 
-import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { Box, Button, Card, CardActions, CardContent, CardHeader, Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, MenuItem, Select, TextField } from '@mui/material'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { createEvent, getCategoryEvent } from '../../services/eventService'
-import { DateTimePicker } from '@mui/x-date-pickers';
 
 import './CreateEvent.css'
 
@@ -57,9 +57,9 @@ const CreateEvent = () => {
     return (
         <Box className='create-main'>
             <Card className='create-card'>
-                <CardHeader title='Nuevo evento' />
+                <CardHeader title='Nuevo evento' sx={{ textAlign: 'start' }} />
                 <CardContent sx={{ display: 'flex', width: '100', padding: '16px', justifyContent: 'center' }}>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '60%'}}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '65%' }}>
                         <TextField
                             required
                             label='Nombre del evento'
@@ -70,28 +70,18 @@ const CreateEvent = () => {
                         <TextField
                             label='Descripción (Opcional)'
                             multiline
-                            rows={5}
+                            rows={8}
                             sx={{ marginBottom: '24px' }}
                             onChange={(e) => handleInputChange('description', e.target.value)}
                         />
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                            <FormGroup sx={{ width: '20%'}}>
-                                <FormControlLabel control={<Checkbox />} label='Gratuito' onChange={(e) => {handleInputChange('isFree', e.target.checked)}}/>
-                                <FormControlLabel control={<Checkbox />} label='Accesible' onChange={(e) => {handleInputChange('isAccessible', e.target.checked)}}/>
+                        <Box sx={{ display: 'flex', gap: '16px' }}>
+                            <FormGroup sx={{ width: '20%', flexWrap: 'nowrap', flexDirection: 'row', gap: '24px' }}>
+                                <FormControlLabel className='checkbox' control={<Checkbox />} label='Gratuito' onChange={(e) => { handleInputChange('isFree', e.target.checked) }} />
+                                <FormControlLabel className='checkbox' control={<Checkbox />} label='Accesible' onChange={(e) => { handleInputChange('isAccessible', e.target.checked) }} />
                             </FormGroup>
-                            <Box sx={{ display: 'flex' }}>
-                                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='es'>
-                                    <DateTimePicker
-                                        required
-                                        label='Fecha y hora *'
-                                        timezone='UTC'
-                                        onChange={(e) => {handleInputChange('date', e.toISOString())}}   
-                                    />
-                                </LocalizationProvider>
-                            </Box>
                         </Box>
                     </Box>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '30%', marginLeft: '24px'}}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', width: '35%', marginLeft: '24px' }}>
                         <FormControl fullWidth >
                             <InputLabel required>Categoría</InputLabel>
                             <Select
@@ -102,6 +92,7 @@ const CreateEvent = () => {
                                 sx={{ marginBottom: '24px' }}
                             >
                                 <MenuItem value={''}>--Selecciona una categoría--</MenuItem>
+
                                 {categories.map((category) => (<MenuItem key={category.id} value={`${category.id}`}>{category.name}</MenuItem>))}
                             </Select>
                         </FormControl>
@@ -112,22 +103,33 @@ const CreateEvent = () => {
                             variant='outlined'
                             sx={{ marginBottom: '24px' }}
                             onChange={(e) => handleInputChange('place', e.target.value)}
-                        />                            
-                        <TextField
-                            required
-                            type='number'
-                            label='Edad mínima'
-                            variant='outlined'
-                            sx={{ marginBottom: '24px' }}
-                            onChange={(e) => handleInputChange('ageMin', e.target.value)}
                         />
-                        <TextField
-                            required
-                            type='number'
-                            label='Edad máxima'
-                            variant='outlined'
-                            onChange={(e) => handleInputChange('ageMax', e.target.value)}
-                        />
+                        <Box
+                            sx={{ display: 'flex', gap: '24px', marginBottom: '24px' }}
+                        >
+                            <TextField
+                                required
+                                type='number'
+                                label='Edad mínima'
+                                variant='outlined'
+                                onChange={(e) => handleInputChange('ageMin', e.target.value)}
+                            />
+                            <TextField
+                                required
+                                type='number'
+                                label='Edad máxima'
+                                variant='outlined'
+                                onChange={(e) => handleInputChange('ageMax', e.target.value)}
+                            />
+                        </Box>
+                        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='es'>
+                            <DateTimePicker
+                                required
+                                label='Fecha y hora *'
+                                timezone='UTC'
+                                onChange={(e) => { handleInputChange('date', e.toISOString()) }}
+                            />
+                        </LocalizationProvider>
                     </Box>
                 </CardContent>
                 <CardActions sx={{ display: 'flex', justifyContent: 'end' }}>
