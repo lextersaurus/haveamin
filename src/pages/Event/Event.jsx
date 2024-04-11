7fac12c03050d063d64439f036565238f58ba42e
Fabio Herrera <fabio_hd_8@hotmail.com>
2024-04-11 10:54:49 +0100

    pagina de evento estilada

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -19,7 +19,6 @@ const EventPage = () => {
   const [event, setEvent] = useState({});
   const [userJoin, setUserJoin] = useState();
   const [events, setEvents] = useState([]);
-
   const { eventId } = useParams();
 
   const handleEvent = async () => {
@@ -65,98 +64,105 @@ const EventPage = () => {
   const isUserJoined = events.some((userEvent) => userEvent.id === event.id);
 
   return (
-    
+    <Box
+      sx={{
+        width: "100vw",
+        height: "90vh",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: "16px",
+        justifyContent: "center",
+      }}
+    >
       <Box
         sx={{
-          width: "100vw",
           display: "flex",
-          apgnItems: "center",
-          flexDirection: "column",
-          padding: "16px",
+          maxWidth: "1000px",
+          width: "80%",
+          gap: "24px",
         }}
       >
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            minWidth: "90%",
-          }}
-        >
-          <div className="eventCard"></div>
-          <Card
-            sx={{ display: "flex", width: "90%", justifyContent: "center" }}
-          >
-            <CardHeader title={event.name} />
-          </Card>
-        </Box>
-
-        <Card sx={{marginTop:'16px' }}>
-
-
-          <CardContent  sx={{
-                display: "flex",
-                flexDirection: "column",
-                textAlign:'left',
-                justifyContent: "center",
-                margin: "16px",
-                padding: "40px",
-                alignItems:'end',
-              }}>
-            
-              <div className="photo"></div>
-                <h2> Nombre Usuario</h2>
-              
-              <p>
-                <Place sx={{ fontSize: "medium" }} /> {event.place}
-              </p>
-              <p>
-                <Event sx={{ fontSize: "medium" }} /> {formatDate(event.date)}
-              </p>
-              <p>
-                <b>Rango de edad: </b>
-                {event.ageMin} - {event.ageMax}
-              </p>
-              {!event.isFree ? (
-                <p className="red">Evento de pago</p>
-              ) : (
-                <p className="green">Evento gratuito</p>
-              )}
-              <div>
-                <Accessible />{" "}
-                {event.isAccessible ? (
-                  <Check color="success" />
-                ) : (
-                  <Close color="error" />
-                )}
-              </div>
-              
-           
-          </CardContent>
-          <CardActions sx={{display:'flex', justifyContent:'end', marginRight:'40px' }}>
-            {isUserJoined ? (
-              <Button
-                variant="outlined"
-                onClick={() => {
-                  handleOnQuit(event.id);
-                }}
-              >
-                Salir
-              </Button>
-            ) : (
-              <Button
-                variant="contained"
-                onClick={() => {
-                  handleOnJoin(event.id);
-                }}
-              >
-                Unirse
-              </Button>
-            )}
-          </CardActions>
+        <div className="eventCard"></div>
+        <Card sx={{ display: "flex", width: "90%", justifyContent: "center" }}>
+          <CardHeader title={event.name} />
         </Card>
       </Box>
-    
+
+      <Card
+        sx={{
+          marginTop: "16px",
+          maxWidth: "1000px",
+          width: "80%",
+          padding: "24px",
+        }}
+      >
+        <CardContent
+          sx={{
+            display: "flex",
+            textAlign: "left",
+            justifyContent: "space-between",
+            padding: "16px",
+          }}
+        >
+          <Box>
+            <div>
+              <p>{event.description}</p>
+            </div>
+          </Box>
+          <Box>
+            <div className="photo"></div>
+            <h2> Nombre Usuario</h2>
+
+            <p>
+              <Place sx={{ fontSize: "medium" }} /> {event.place}
+            </p>
+            <p>
+              <Event sx={{ fontSize: "medium" }} /> {formatDate(event.date)}
+            </p>
+            <p>
+              <b>Rango de edad: </b>
+              {event.ageMin} - {event.ageMax}
+            </p>
+            {!event.isFree ? (
+              <p className="red">Evento de pago</p>
+            ) : (
+              <p className="green">Evento gratuito</p>
+            )}
+            <div>
+              <Accessible />{" "}
+              {event.isAccessible ? (
+                <Check color="success" />
+              ) : (
+                <Close color="error" />
+              )}
+            </div>
+          </Box>
+        </CardContent>
+
+        <CardActions sx={{ display: "flex", justifyContent: "end" }}>
+          {isUserJoined ? (
+            <Button
+              variant="outlined"
+              onClick={() => {
+                handleOnQuit(event.id);
+              }}
+            >
+              Salir
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              onClick={() => {
+                handleOnJoin(event.id);
+              }}
+            >
+              Unirse
+            </Button>
+          )}
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
