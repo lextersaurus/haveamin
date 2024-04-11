82e947756efad73958d91d2b2baf03721c4e04aa
Esther Mesa <estmesher@gmail.com>
2024-04-11 03:25:38 +0100

    create service to get event created by a certain user

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,25 +1,25 @@
-import { api } from "./config"
+import { api } from './config'
 
-export const getCategoryEvent = async () =>{
+export const getCategoryEvent = async () => {
     const { data } = await api.get('/category/showall',{
-            headers: {
-                'Authorization': localStorage.getItem('token')
-            }
-        })
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
     return data
 }
 
-export const getAllEvents = async () =>{
+export const getAllEvents = async () => {
     const { data } = await api.get('/event/showall',{
-            headers: {
-                'Authorization': localStorage.getItem('token')
-            }
-        })
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
     return data
 }
 
-export const getEventByCategory = async (categoryId) =>{
-    const { data } = await api.get('/category/'+ categoryId +'/events',{
+export const getEventByCategory = async (categoryId) => {
+    const { data } = await api.get(`/category/${categoryId}/events`,{
         headers: {
             'Authorization': localStorage.getItem('token')
         }
@@ -51,6 +51,5 @@ export const createEvent = async (eventData) => {
             'Authorization': localStorage.getItem('token')
         }
     })
-    console.log(data)
     return data
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,19 @@
-import { api } from "./config"
+import { api } from './config'
 
-export const getUserEvent = async () =>{
+export const getUserEvent = async () => {
     const { data } = await api.get('user/events',{
-            headers: {
-                'Authorization': localStorage.getItem('token')
-            }
-        })/* 
-    console.log(data) */
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
     return data
- }
+}
+
+export const getCreatedEvents = async (userId) => {
+    const { data } = await api.get(`event/created/${userId}`, {
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
+    return data
+}
