4a114ac046bb0522ddefaa653922966472dc3a85
Esther Mesa <estmesher@gmail.com>
2024-04-12 14:35:38 +0100

    EVENT - Change border radius of cards

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -89,7 +89,7 @@ const EventPage = () => {
         }}
       >
         <div className='eventCard'></div>
-        <Card sx={{ display: 'flex', width: '90%', justifyContent: 'center' }}>
+        <Card sx={{ display: 'flex', width: '90%', justifyContent: 'center', borderRadius: '16px' }}>
           <CardHeader title={event.name} />
         </Card>
       </Box>
@@ -101,6 +101,7 @@ const EventPage = () => {
           width: '80%',
           padding: '24px',
           height: 'fit-content',
+          borderRadius: '16px'
         }}
       >
         <CardContent
